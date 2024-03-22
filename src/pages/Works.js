9bc5a4bffa164565f93989d2fb551c1b8eadd004
karinastrand <karinastrand@yahoo.se>
2024-03-22 20:11:19 +0100

    project added

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Img1 from '../Images/munamii.jpg'
-
+import Img2 from '../Images/aworldofbook.png'
 import {Container, Row, Col, Button, label,Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Works() {
@@ -10,10 +10,17 @@ export default function Works() {
             <h3>My latest Projects</h3>
         </Row>
         <Row>
-            <Col>
+            <Col >
+            <div class="mb-2">
            <a href='https://karinastrand.github.io/munamii-react/' target='_blank'  >
                 <img src={Img1} alt="munamii" style={{width:300}}/>
            </a>
+           </div>
+           <div>
+           <a href='https://karinastrand.github.io/aworldofbooks/' target='_blank'  >
+                <img src={Img2} alt="aworldofbooks" style={{width:300}}/>
+           </a>
+           </div>
            </Col>
            <Col>
            </Col>
